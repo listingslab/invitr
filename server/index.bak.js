52683217c4a7255bb5dffb23c854f38564ce9ce9
listingslab <listingslab@gmail.com>
2016-04-21 20:41:24 +0800

    Restart Koa Router

--- server/index.bak.js
+++ server/index.bak.js
@@ -1,15 +1,18 @@
 "use strict";
 
-const 	appname 	= 'invitr',
-		env			= process.env,
-		host 		= env.NODE_IP || 'localhost',
-		port 		= env.NODE_PORT || 1976,
-		clear		= require('clear'),
-		colors		= require('colors'),
-		koa			= require('koa');
+let env			= process.env;
+let appname 	= 'invitr';
+let host 		= env.NODE_IP || 'localhost';
+let port 		= env.NODE_PORT || 1976;
+let clear		= require('clear');
+let colors		= require('colors');
+let koa			= require('koa');
+var router 		= require('koa-router');
+var mount 		= require('koa-mount');
 
-let app = koa();
+const app = koa();
 
+// Request Logger Middleware
 function *reqlogger(next){
 	var timeIs = new Date ();
 	timeIs = timeIs.getHours() + ':' + timeIs.getMinutes() + ' ' + timeIs.getSeconds();
@@ -17,6 +20,7 @@ function *reqlogger(next){
 	yield next;
 }
 app.use(reqlogger);
+
 
 
 app.listen( port, host, function () {
