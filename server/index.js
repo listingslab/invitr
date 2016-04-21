5ab3968f7b00c6a744d36382f6eef247e3d56808
listingslab <listingslab@gmail.com>
2016-04-21 14:34:19 +0800

    Server ready to build

--- server/index.js
+++ server/index.js
@@ -5,16 +5,17 @@ let appname 	= 'invitr';
 let host 		= env.NODE_IP || 'localhost';
 let port 		= env.NODE_PORT || 1976;
 let colors		= require('colors');
-let fs			= require('fs-extra');
-let serve		= require('koa-static');
+let koa			= require('koa');
 let body		= require('koa-better-body');
 let request 	= require('koa-request');
-let koa			= require('koa');
+let serve		= require('koa-static');
 
 const app = koa();
 
 app.use(serve('public'));
 app.use(body({formLimit:'10mb'}));
+
+//http://code.tutsplus.com/tutorials/introduction-to-generators-koajs-part-2--cms-21756
 
 app.listen( port, host, function () {
 	console.log('~~~~~~~~~~~~~~~~| ' + appname + ' on http://' + host + ':' +  port + ' |~~~~~~~~~~~~~~~~');
