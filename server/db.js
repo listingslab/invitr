e43d633affc8c9d8d1b1d1a45e9ba2066b4ebc3a
listingslab <listingslab@gmail.com>
2016-04-21 14:18:20 +0800

    Remove routing. We're going to start again with that.

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
 var loki		= require('lokijs')
 
-var db = new loki('content.json');
+var db = new loki('loki.json');
+
 db.loadDatabase({}, function () {
 	if (db.getCollection('settings') === null){
 		var content = db.addCollection('settings');
 		db.saveDatabase();
-		console.log('New Loki Database created!! /content.json'.bgYellow);
+		console.log('New Loki Database created!! /loki.json'.bgYellow);
 	}
 });
--- server/index.js
+++ server/index.js
@@ -4,28 +4,17 @@ let env			= process.env;
 let appname 	= 'invitr';
 let host 		= env.NODE_IP || 'localhost';
 let port 		= env.NODE_PORT || 1976;
-
 let colors		= require('colors');
 let fs			= require('fs-extra');
 let serve		= require('koa-static');
-let routing		= require('koa-routing');
 let body		= require('koa-better-body');
 let request 	= require('koa-request');
-let koa			= require('koa')
-let app = koa();
+let koa			= require('koa');
 
-app.use(function* (next) {
-	yield next;
-});
-
+const app = koa();
 
 app.use(serve('public'));
 app.use(body({formLimit:'10mb'}));
-app.use(routing(app));
-
-require('./routes/restapi')(app);
-require('./routes/html')(app);
-
 
 app.listen( port, host, function () {
 	console.log('~~~~~~~~~~~~~~~~| ' + appname + ' on http://' + host + ':' +  port + ' |~~~~~~~~~~~~~~~~');
