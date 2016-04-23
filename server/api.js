9719534b6a33425f41e2a3c1ef6904213088e035
listingslab <listingslab@gmail.com>
2016-04-23 13:45:32 +0800

    start developing mongo

--- server/api.js
+++ server/api.js
@@ -2,18 +2,26 @@
 "use strict";
 
 const 	koa 	= require('koa'),
-		route 	= require('koa-route');
+		route 	= require('koa-route'),
+		utils 	= require('./utils');
 
 var app = module.exports = koa();
 
 // prefix: api
 app.use(route.get("/", apithing));
-app.use(route.get("/:bit", onething));
+app.use(route.get("/nest/:bit", onething));
+app.use(route.get("/test-mongo", testMongo));
+
+function *testMongo() {
+	this.body = {
+		success: true
+	};
+};
 
 function *apithing() {
 	this.body = {
 		method:this.req.method,
-		url:this.req.url,
+		time:utils.time(),
 	};
 };
 
--- server/utils.js
+++ server/utils.js
@@ -1,17 +1,18 @@
 // Listingslab Server Utilities
 "use strict";
 
-
+// A nice little logger to we can keep track of server requests
 module.exports.requestLogger = function *(next) {
 	console.log('%s %s %s %s', this.req.method, 'Request to', this.req.url , '@ ' + getTime ());
 	yield next;
 }
 
-// Returns the time
+// Exposes the time
 module.exports.time = function () {
   return getTime();
 };
 
+// Works out the time
 function getTime (){
 	let time = new Date ();
  	time = time.getHours() + ':' + time.getMinutes() + ' ' + time.getSeconds() + 's';
