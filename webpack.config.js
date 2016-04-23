156093f590d11456ce4db40fc1277f7f3c5657d1
listingslab <listingslab@gmail.com>
2016-04-23 16:11:09 +0800

    Start developing the React App

--- webpack.config.js
+++ webpack.config.js
@@ -4,3 +4,62 @@
 */
 
 "use strict";
+
+
+const   clear    = require('clear'),
+        colors    = require('colors'),
+        path = require('path'),
+        webpack = require('webpack');
+
+
+clear ();
+console.log('Running Webpack'.redBG);
+
+const PATHS = {
+  app: path.join(__dirname, 'react-app/App.jsx'),
+  build: path.join(__dirname, 'public')
+};
+
+module.exports = {
+	
+  entry: {
+    app: PATHS.app
+	},
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  output: {
+    path: PATHS.build,
+    filename: 'js/react-app.js'
+  },
+
+  module: {
+
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['eslint'],
+        include: PATHS.app
+      }
+    ],
+
+    loaders: [
+      
+      { 
+        test: /\.css$/, 
+        loader: 'style!css' 
+      },
+
+      {
+        test: /\.jsx?$/,
+        loader: ['babel'],
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ],
+  }
+}
