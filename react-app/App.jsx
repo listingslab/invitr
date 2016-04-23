7ee8385f2eefe3ec3bf234c06c21b3331bd88304
listingslab <listingslab@gmail.com>
2016-04-23 16:36:16 +0800

    I Am Content

--- react-app/App.jsx
+++ react-app/App.jsx
@@ -1,1 +1,21 @@
-console.log('APP');
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { browserHistory, Router, Route, IndexRoute, Link } from 'react-router';
+
+class App extends Component {
+	render() {
+		return (
+	      <div>
+	        I am content
+	      </div>
+	    );
+	}
+}
+
+render((
+  <Router history={ browserHistory }>
+    <Route path="/" component={ App }>
+    	the other place
+    </Route>
+  </Router>
+), document.getElementById('react-app'));
